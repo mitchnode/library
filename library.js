1b2a9db6a87bcd794bc89f49d5f3cdfcad016f26
mitchnode <70460735+mitchnode@users.noreply.github.com>
2024-02-12 22:11:01 +1100

    Added Card layout and filled array data

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+const cards = document.getElementById("cards");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -5,22 +7,40 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return title + " by " + author + ", " + pages + " pages, " + ((read) ? "Read" : "not read yet");
+    this.readinfo = function() {
+        return ((read) ? "Read" : "Not Read Yet");
+    }
+    this.generateCard = function() {
+        const card = document.createElement("div");
+        card.className = "card";
+        cards.appendChild(card);
+        const titletext = document.createElement("p");
+        const authortext = document.createElement("p");
+        const pagestext = document.createElement("p");
+        const readtext = document.createElement("p");
+        titletext.textContent = "Title: " + this.title;
+        authortext.textContent = "Author: " + this.author;
+        pagestext.textContent = "Pages: " + this.pages;
+        readtext.textContent = this.readinfo();
+        card.appendChild(titletext);
+        card.appendChild(authortext);
+        card.appendChild(pagestext);
+        card.appendChild(readtext);
     }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
 }
 
 function getBooks() {
     myLibrary.forEach(book => {
-        // return book object as array of strings
-        //(Format for display on page)
+          book.generateCard();
     });
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hitchhikers Guide to the Galaxy", "Douglas Adams", 342, true);
+addBookToLibrary("The Magician", "Raymond E. Fiest", 580, true);
 
-console.log(theHobbit.info());
+getBooks();
